a0e3efcf95a09fce3bcb17b522ba080282f4cd9d
giannibussoletti <bussolettigianni@gmail.com>
2026-06-03 09:26:09 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+// Packages
+// Sono delle cartelle che raggruppano in maniera ordinata le diverse classi
+// attinenti fra di loro, diversi packages permettono di avere due classi con
+// lo stesso nome basta che siano in packages.
+
+// Se una classe vuole utilizzare un'altra classe che si trova in un package differente, deve importarla:
+//  - Librerie Standard: Abbiamo già visto import java.util.Scanner;. Qui stiamo dicendo a Java di andare nel
+//  package java.util a prendere lo strumento Scanner
+// - Import Automatici: II package java.lang (che contiene String, System, Integer) è l'unico importato
+// automaticamente in ogni file
